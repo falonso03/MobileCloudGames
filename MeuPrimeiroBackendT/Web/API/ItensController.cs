fd3848e87afc2e7cf0a8b541e7dd8322ddd5117a
thiagoamplatz <thiagoamplatz@hotmail.com>
2017-10-23 17:09:55 -0200

    Criando WebAPI

---
+++ MeuPrimeiroBackendT/Web/API/ItensController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Web.Models;
+using Web.Models.Contexto;
+
+namespace Web.API
+{
+    public class ItensController : ApiController
+    {
+        private MeuContexto db = new MeuContexto();
+
+        // GET: api/Itens
+        public IQueryable<Item> GetItems()
+        {
+            return db.Items;
+        }
+
+        // GET: api/Itens/5
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult GetItem(int id)
+        {
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        // PUT: api/Itens/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutItem(int id, Item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != item.ItemID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Itens
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult PostItem(Item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Items.Add(item);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = item.ItemID }, item);
+        }
+
+        // DELETE: api/Itens/5
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult DeleteItem(int id)
+        {
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            db.Items.Remove(item);
+            db.SaveChanges();
+
+            return Ok(item);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ItemExists(int id)
+        {
+            return db.Items.Count(e => e.ItemID == id) > 0;
+        }
+    }
+}
