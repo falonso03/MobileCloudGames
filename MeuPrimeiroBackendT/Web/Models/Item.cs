17fd1d79af56676a4aeb4bd6abf1f36608a6dbd6
thiagoamplatz <thiagoamplatz@hotmail.com>
2017-10-23 15:43:36 -0200

    Criando as modelos (classes das tabelas)

---
+++ MeuPrimeiroBackendT/Web/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿namespace Web.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int Raridade { get; set; }
+
+        public int DanoMaximo { get; set; }
+
+        //Relacionamento item -- TipoItem
+
+        public int TipoItemID { get; set; }
+
+        public virtual TipoItem _TipoItem { get; set; }
+
+
+
+    }
+}
---
+++ MeuPrimeiroBackendT/Web/Models/TipoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Models
+{
+    public class TipoItem
+    {
+
+        public int TipoItemID { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
